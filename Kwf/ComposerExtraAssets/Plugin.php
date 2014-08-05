b9ee183f4103fb8b3fcb10253f2cdd0d21142f53
Niko Sams <ns@vivid-planet.com>
2014-08-05 08:06:56 +0200

    install bower components in vendor sub directory

--- Kwf/ComposerExtraAssets/Plugin.php
+++ Kwf/ComposerExtraAssets/Plugin.php
@@ -77,6 +77,10 @@ class Plugin implements PluginInterface, EventSubscriberInterface
                 'dependencies' => $requireBower,
             );
             file_put_contents('bower.json', json_encode($packageJson));
+            $config = array(
+                'directory' => 'vendor/bower_components'
+            );
+            file_put_contents('.bowerrc', json_encode($config));
             $this->io->write("");
             $this->io->write("installing bower dependencies...");
             $cmd = "vendor/koala-framework/composer-extra-assets/node_modules/.bin/bower install";
@@ -85,6 +89,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
                 $this->io->write("<error>bower install failed</error>");
             }
             unlink('bower.json');
+            unlink('.bowerrc');
         }
     }
 
